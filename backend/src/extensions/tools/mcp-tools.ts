7605c6ff299859cf951355b8f60c099fa65ff043
Christopher Schädlich <christopher.schaedlich@codecentric.de>
2025-07-21 14:13:26 +0200

    feat: add request headers to mcp tool

--- backend/src/extensions/tools/mcp-tools.ts
+++ backend/src/extensions/tools/mcp-tools.ts
@@ -49,6 +49,7 @@ type ConfigurationAttributes = Record<
 interface Configuration {
   serverName: string;
   endpoint: string;
+  headers?: string;
   transport: Transport;
   schema?: Record<
     // one record per method
@@ -263,7 +264,7 @@ export class MCPToolsExtension implements Extension<Configuration> {
       logo: '<svg fill="currentColor" fill-rule="evenodd" height="1em" style="flex:none;line-height:1" viewBox="0 0 24 24" width="1em" xmlns="http://www.w3.org/2000/svg"><title>ModelContextProtocol</title><path d="M15.688 2.343a2.588 2.588 0 00-3.61 0l-9.626 9.44a.863.863 0 01-1.203 0 .823.823 0 010-1.18l9.626-9.44a4.313 4.313 0 016.016 0 4.116 4.116 0 011.204 3.54 4.3 4.3 0 013.609 1.18l.05.05a4.115 4.115 0 010 5.9l-8.706 8.537a.274.274 0 000 .393l1.788 1.754a.823.823 0 010 1.18.863.863 0 01-1.203 0l-1.788-1.753a1.92 1.92 0 010-2.754l8.706-8.538a2.47 2.47 0 000-3.54l-.05-.049a2.588 2.588 0 00-3.607-.003l-7.172 7.034-.002.002-.098.097a.863.863 0 01-1.204 0 .823.823 0 010-1.18l7.273-7.133a2.47 2.47 0 00-.003-3.537z"></path><path d="M14.485 4.703a.823.823 0 000-1.18.863.863 0 00-1.204 0l-7.119 6.982a4.115 4.115 0 000 5.9 4.314 4.314 0 006.016 0l7.12-6.982a.823.823 0 000-1.18.863.863 0 00-1.204 0l-7.119 6.982a2.588 2.588 0 01-3.61 0 2.47 2.47 0 010-3.54l7.12-6.982z"></path></svg>',
       description: this.i18n.t('texts.extensions.mcpTools.description'),
       type: 'tool',
-      triggers: ['endpoint'],
+      triggers: ['endpoint', 'transport', 'headers'],
       arguments: {
         serverName: {
           type: 'string',
@@ -285,6 +286,13 @@ export class MCPToolsExtension implements Extension<Configuration> {
           required: false,
           default: 'sse',
           enum: Object.values(Transport),
+        },
+        headers: {
+          type: 'string',
+          title: this.i18n.t('texts.extensions.common.headers'),
+          description: this.i18n.t('texts.extensions.mcpTools.headersHint'),
+          format: 'textarea',
+          required: false,
         },
       },
     };
@@ -393,11 +401,9 @@ export class MCPToolsExtension implements Extension<Configuration> {
 
   private enableElicitRequests(client: Client, context: ChatContext) {
     client.setRequestHandler(ElicitRequestSchema, async (request) => {
-      this.logger.log('Received elicit request', { request });
       const schema = toExtensionArgument(request.params.requestedSchema as JsonSchemaObject, {});
       if (schema) {
         const userResponse = await context.ui.form(request.params.message, schema);
-        this.logger.log('Received ui response', { userResponse });
         return {
           action: userResponse.action,
           content: userResponse.action === FormActionType.ACCEPT ? userResponse.data : undefined,
@@ -498,10 +504,30 @@ export class MCPToolsExtension implements Extension<Configuration> {
     );
 
     const url = new URL(configuration.endpoint);
+
+    const headers =
+      configuration.headers?.split(/\r?\n/).reduce<Record<string, string>>((prev, pair) => {
+        const index = pair.indexOf('=');
+
+        if (index >= 0) {
+          const key = pair.substring(0, index).trim();
+          const value = pair.substring(index + 1).trim();
+          if (key && value) {
+            prev[key.trim()] = value.trim();
+          }
+        }
+
+        return prev;
+      }, {}) ?? {};
+
     const transport =
       configuration.transport === Transport.STREAMABLE_HTTP
-        ? new StreamableHTTPClientTransport(url)
-        : new SSEClientTransport(url);
+        ? new StreamableHTTPClientTransport(url, {
+            requestInit: { headers },
+          })
+        : new SSEClientTransport(new URL(url), {
+            requestInit: { headers },
+          });
     await client.connect(transport);
     const { tools } = await client.request({ method: 'tools/list' }, ListToolsResultSchema);
     return { tools, client };
