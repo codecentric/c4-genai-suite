a495aa7484e5a4854076e3d2cc0a6e65f622a120
Hendrik Schawe <hendrik.schawe@codecentric.de>
2025-09-25 15:48:03 +0200

    feat: use a transaction and handle the empty conversation

--- backend/src/domain/extensions/use-cases/delete-configuration.ts
+++ backend/src/domain/extensions/use-cases/delete-configuration.ts
@@ -1,14 +1,7 @@
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult, Not } from 'typeorm';
-import {
-  ConfigurationEntity,
-  ConfigurationRepository,
-  ConfigurationStatus,
-  ConversationEntity,
-  ConversationRepository,
-} from 'src/domain/database';
+import { DataSource, IsNull, Not } from 'typeorm';
+import { ConfigurationEntity, ConfigurationStatus, ConversationEntity } from 'src/domain/database';
 import { assignDefined } from 'src/lib';
 import { I18nService } from 'src/localization/i18n.service';
 
@@ -19,61 +12,71 @@ export class DeleteConfiguration {
 @CommandHandler(DeleteConfiguration)
 export class DeleteConfigurationHandler implements ICommandHandler<DeleteConfiguration, any> {
   constructor(
+    private dataSource: DataSource,
     private readonly i18n: I18nService,
-    @InjectRepository(ConfigurationEntity)
-    private readonly configurations: ConfigurationRepository,
-    @InjectRepository(ConversationEntity)
-    private readonly conversations: ConversationRepository,
   ) {}
 
   async execute(command: DeleteConfiguration): Promise<any> {
-    let result: DeleteResult;
     const { id } = command;
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
 
-    // So, we first try to just delete the configuration, which works it it was never used.
+    await queryRunner.startTransaction();
     try {
-      result = await this.configurations.delete({ id: id });
-
-      if (!result.affected) {
-        throw new NotFoundException();
-      }
+      // First, we try to actually delete the assistant. This will work if no messages for this assistant exist.
+      // The empty conversation must be deleted, if it references the assistant.
+      await queryRunner.manager.delete(ConversationEntity, { configurationId: id, llm: IsNull(), name: IsNull() });
+      await queryRunner.manager.delete(ConfigurationEntity, { id: command.id });
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+      return;
     } catch (_error) {
-      // If it does not work, because there are still messages referring to them, we mark the configuration as deleted.
+      await queryRunner.rollbackTransaction();
+    }
+
+    await queryRunner.startTransaction();
+    try {
+      // If it does not work, because there are still messages referring to them,
+      // we just mark the configuration as deleted.
       // That will only have the effect that the configuration will not be shown in any user facing menu,
-      // but is still available for the existing messages to show needed information.
-      const entity = await this.configurations.findOne({
+      // but is still available for the existing messages to reference to.
+      // For conversations which have this assistant as the active one, we need to supply a replacement.
+      // We will just use the first enabled assistant. If the user tries to delete the last one, we show an error.
+      const toBeDeleted = await queryRunner.manager.findOne(ConfigurationEntity, {
         where: { id },
         relations: ['conversations'],
       });
-
-      if (!entity) {
+      if (!toBeDeleted) {
         throw new NotFoundException();
       }
-
-      // For conversations which have this assistant as the active one, we need to supply a replacement.
-      // We will just use the first enabled assistant. If the user tries to delete the last one, we show an error.
-      const replacement = await this.configurations.findOne({
+      const replacement = await queryRunner.manager.findOne(ConfigurationEntity, {
         where: { id: Not(id), status: ConfigurationStatus.ENABLED },
         order: { id: 'ASC' },
       });
-
       if (!replacement) {
         throw new BadRequestException(this.i18n.t('texts.chat.errorLastConfigurationdDeleted'));
       }
 
-      if (entity.conversations) {
-        for (const conversation of entity.conversations) {
+      await queryRunner.manager.delete(ConversationEntity, { configurationId: id, llm: IsNull(), name: IsNull() });
+      if (toBeDeleted.conversations) {
+        for (const conversation of toBeDeleted.conversations) {
           conversation.configuration = replacement;
-          await this.conversations.save(conversation);
+          await queryRunner.manager.save(ConversationEntity, conversation);
         }
       }
-
-      assignDefined(entity, {
+      assignDefined(toBeDeleted, {
         status: ConfigurationStatus.DELETED,
         conversations: [],
       });
 
-      await this.configurations.save(entity);
+      await queryRunner.manager.save(ConfigurationEntity, toBeDeleted);
+
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
     }
   }
 }
