a10a428ad0cded4bc54e8f59ac1589390ba99606
Hendrik Schawe <hendrik.schawe@codecentric.de>
2025-09-25 14:05:57 +0200

    feat: display state of assistants in the frontend

--- backend/src/domain/extensions/use-cases/delete-configuration.ts
+++ backend/src/domain/extensions/use-cases/delete-configuration.ts
@@ -1,9 +1,16 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DeleteResult } from 'typeorm';
-import { ConfigurationEntity, ConfigurationRepository, ConfigurationStatus } from 'src/domain/database';
+import { DeleteResult, Not } from 'typeorm';
+import {
+  ConfigurationEntity,
+  ConfigurationRepository,
+  ConfigurationStatus,
+  ConversationEntity,
+  ConversationRepository,
+} from 'src/domain/database';
 import { assignDefined } from 'src/lib';
+import { I18nService } from 'src/localization/i18n.service';
 
 export class DeleteConfiguration {
   constructor(public readonly id: number) {}
@@ -12,15 +19,18 @@ export class DeleteConfiguration {
 @CommandHandler(DeleteConfiguration)
 export class DeleteConfigurationHandler implements ICommandHandler<DeleteConfiguration, any> {
   constructor(
+    private readonly i18n: I18nService,
     @InjectRepository(ConfigurationEntity)
     private readonly configurations: ConfigurationRepository,
+    @InjectRepository(ConversationEntity)
+    private readonly conversations: ConversationRepository,
   ) {}
 
   async execute(command: DeleteConfiguration): Promise<any> {
     let result: DeleteResult;
     const { id } = command;
 
-    // So, we first try to just delete the configuration.
+    // So, we first try to just delete the configuration, which works it it was never used.
     try {
       result = await this.configurations.delete({ id: id });
 
@@ -33,14 +43,34 @@ export class DeleteConfigurationHandler implements ICommandHandler<DeleteConfigu
       // but is still available for the existing messages to show needed information.
       const entity = await this.configurations.findOne({
         where: { id },
+        relations: ['conversations'],
       });
 
       if (!entity) {
         throw new NotFoundException();
       }
 
+      // For conversations which have this assistant as the active one, we need to supply a replacement.
+      // We will just use the first enabled assistant. If the user tries to delete the last one, we show an error.
+      const replacement = await this.configurations.findOne({
+        where: { id: Not(id), status: ConfigurationStatus.ENABLED },
+        order: { id: 'ASC' },
+      });
+
+      if (!replacement) {
+        throw new BadRequestException(this.i18n.t('texts.chat.errorLastConfigurationdDeleted'));
+      }
+
+      if (entity.conversations) {
+        for (const conversation of entity.conversations) {
+          conversation.configuration = replacement;
+          await this.conversations.save(conversation);
+        }
+      }
+
       assignDefined(entity, {
         status: ConfigurationStatus.DELETED,
+        conversations: [],
       });
 
       await this.configurations.save(entity);
