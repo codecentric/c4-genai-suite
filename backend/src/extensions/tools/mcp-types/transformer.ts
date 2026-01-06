176a4b05ca71ac5d2e3baa249bf92e78c7fa1349
Hendrik Schawe <hendrik.schawe@codecentric.de>
2026-01-06 10:25:47 +0100

    chore(deps): use supplied types instead of infering from schema

--- backend/src/extensions/tools/mcp-types/transformer.ts
+++ backend/src/extensions/tools/mcp-types/transformer.ts
@@ -1,12 +1,11 @@
-import { CallToolResultSchema } from '@modelcontextprotocol/sdk/types.js';
-import { z } from 'zod';
+import { CallToolResult } from '@modelcontextprotocol/sdk/types.js';
 import { Source } from 'src/domain/chat';
 import { C4JsonType, convertC4JsonToSource, convertC4JsonToText } from './c4-json.type';
 
 export type MCPText = { type: 'text'; text: string };
 export type TransformedResponse = { content: MCPText[]; sources: Source[] };
 
-export function transformMCPToolResponse(res: z.infer<typeof CallToolResultSchema>): TransformedResponse {
+export function transformMCPToolResponse(res: CallToolResult): TransformedResponse {
   const c4Content = res.content.filter((x) => x.type === 'resource' && x.resource.mimeType === 'application/x-c4-json-v1');
   if (!c4Content.length) {
     const content = res.content.filter((x) => x.type === 'text');
@@ -22,8 +21,12 @@ export function transformMCPToolResponse(res: z.infer<typeof CallToolResultSchem
         return prev;
       }
 
+      if (!('text' in curr.resource)) {
+        return prev;
+      }
+
       const { text } = curr.resource;
-      const c4JsonData = JSON.parse(text as string) as C4JsonType;
+      const c4JsonData = JSON.parse(text) as C4JsonType;
       prev.sources.push(convertC4JsonToSource(c4JsonData));
       prev.content.push(convertC4JsonToText(c4JsonData));
 
